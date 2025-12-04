d1f00273a456b9ebcfc0cacb51944a8b9df964f2
Robbie Carvalho <130287531+R0bbyt3@users.noreply.github.com>
2025-12-04 14:24:42 -0300

    colocando fonte montserrat

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,17 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AuthGuard from "@/components/auth-guard";
 import { SidebarProvider } from "@/components/side-bar/sidebar_provider";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+import { Montserrat } from "next/font/google";
+const montserrat = Montserrat({
+  variable: "--font-montserrat",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800"],
 });
 
 export const metadata: Metadata = {
@@ -27,7 +22,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${montserrat.variable} antialiased`}
+        style={{ fontFamily: "var(--font-montserrat)" }}
       >
         <SidebarProvider>
           <AuthGuard>{children}</AuthGuard>
