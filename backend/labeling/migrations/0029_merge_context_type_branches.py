a175ccd0e311268a5166d4afe6fdfaf0035ca59b
joao paulo <joaopaulosoareslopes22@gmail.com>
2026-03-19 16:41:58 -0300

    merge with main branch , db migrations fix ANNOTAISE-131

---
+++ backend/labeling/migrations/0029_merge_context_type_branches.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('labeling', '0027_alter_labelingelement_context_type'),
+        ('labeling', '0028_merge_0025_0027'),
+    ]
+
+    operations = [
+    ]
